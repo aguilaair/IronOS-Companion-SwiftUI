f8005ea65ade9317e38f34a7f3e35f71af6d1176
Eduardo M <contact@eduardom.dev>
2025-05-26 21:50:14 -0700

    Initial Commit

---
+++ IronOS Companion/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  IronOS Companion
+//
+//  Created by Eduardo Moreno Adanez on 5/26/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
